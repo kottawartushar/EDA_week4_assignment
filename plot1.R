2db55fa05c39c712c01677d4b7e9863fc22e09f6
Kottawar <Tushar.Kottawar@Honeywell.com>
2020-07-17 00:08:23 +0530

    uploaded plot1.R and plot1.png

---
+++ plot1.R
@@ -0,0 +1,22 @@
+scc_pm25 <- readRDS("summarySCC_PM25.rds")
+sourceCC <- readRDS("Source_Classification_Code.rds")
+
+head(scc_pm25)
+str(scc_pm25)
+
+head(sourceCC)
+str(sourceCC)
+
+yearSum <- aggregate(scc_pm25$Emissions, by = list(scc_pm25$year), FUN = sum)
+yearSum
+
+names(yearSum)[1] <- "year"
+names(yearSum)[2] <- "total_emissions"
+yearSum
+
+dev.copy(png, file = "plot1.png", width = 480, height = 480)
+
+plot(yearSum$year, yearSum$total_emissions, xlab = "Year", ylab = "Total Emissions", 
+     col = "blue", type = "l")
+
+dev.off()
